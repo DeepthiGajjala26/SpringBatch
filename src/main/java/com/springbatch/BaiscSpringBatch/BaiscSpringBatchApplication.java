45a69f07d281fcd3faec87b30289bbe9723452eb
TejuAkshi <ntejeswarreddy@gmail.com>
2024-06-13 14:17:29 +0530

    committed sucessfully

---
+++ src/main/java/com/springbatch/BaiscSpringBatch/BaiscSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.BaiscSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiscSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiscSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springbatch/BaiscSpringBatch/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.springbatch.BaiscSpringBatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+    @GetMapping("/get")
+    public String test(){
+        return "started controller";
+    }
+
+}
