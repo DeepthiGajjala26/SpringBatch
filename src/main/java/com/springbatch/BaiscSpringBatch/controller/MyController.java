5f31b70344f090c5f640a85b396e15d43b818809
DeepthiGajjala26 <deepthigajjala26@gmail.com>
2024-06-20 12:27:21 +0530

    "commited"

--- src/main/java/com/springbatch/BaiscSpringBatch/controller/MyController.java
+++ src/main/java/com/springbatch/BaiscSpringBatch/controller/MyController.java
@@ -1,5 +1,15 @@
 package com.springbatch.BaiscSpringBatch.controller;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MyController {
 
-    @GetMapping("/get")
-    public String test(){
-        return "started controller";
-    }
+//    @Autowired
+//    private JobLauncher jobLauncher;
+//
+//    @Autowired
+//    private Job job;
+@GetMapping("/get")
+public String test(){
+    return "started controller";
+}
     @GetMapping("/test")
     public String deepu(){
         return "end controller";
@@ -24,5 +39,151 @@ public class MyController {
     public String pull(){
         return "pull sucessfully";
     }
+    @GetMapping("/test1")
+    public String test1(){
+        return "test1 method";
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  @GetMapping("/batch")
+//    public String StartJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+//
+//      jobLauncher.run(job, new JobParameters());
+//      return "Job has been executed successfully!";
+//
+//  }
 
 }
